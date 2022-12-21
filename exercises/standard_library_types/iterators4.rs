c768bb0358dd6670c49b762f51baf37e2c59eeb2
Tyler Knapp <knapptr@gmail.com>
2022-12-21 12:17:03 -0500

    Solves through box

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,10 +1,14 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
+    (1..=num).rev().product()
+    // match num {
+    //     0 => 1,
+    //     1 => 1,
+    //     _ => num * factorial(num - 1),
+    // }
     // Do not use:
     // - return
     // Try not to use:
